5ccc8cc2f45ebb24f4d5bd6bcfc92f77d33dc714
ArtamonovEvgenii <evgenyi.artamonov@gmail.com>
2026-01-03 17:00:36 +0300

    [HW01] add timezone info

--- internal/controller/grpc/dto.go
+++ internal/controller/grpc/dto.go
@@ -4,19 +4,28 @@ import (
 	"time"
 
 	"google.golang.org/genproto/googleapis/type/datetime"
+	"google.golang.org/protobuf/types/known/durationpb"
 )
 
 func responseDateTime(t time.Time) *datetime.DateTime {
-	//_, timeOffset := entityNote.UpdatedAt.Zone()
+	// v1
+	_, timeOffsetSec := t.Zone()
+	tz := &datetime.DateTime_UtcOffset{
+		UtcOffset: &durationpb.Duration{Seconds: int64(timeOffsetSec)},
+	}
+
+	// v2
+	//timeZoneName, _ := t.Zone()
+	//tz := &datetime.DateTime_TimeZone{TimeZone: &datetime.TimeZone{Id: timeZoneName}}
 
 	return &datetime.DateTime{
-		Year:    int32(t.Year()),
-		Month:   int32(t.Month()),
-		Day:     int32(t.Day()),
-		Hours:   int32(t.Hour()),
-		Minutes: int32(t.Minute()),
-		Seconds: int32(t.Second()),
-		Nanos:   int32(t.Nanosecond()),
-		//TimeOffset: datetime.TimeOffset{}, // todo: set time offset
+		Year:       int32(t.Year()),
+		Month:      int32(t.Month()),
+		Day:        int32(t.Day()),
+		Hours:      int32(t.Hour()),
+		Minutes:    int32(t.Minute()),
+		Seconds:    int32(t.Second()),
+		Nanos:      int32(t.Nanosecond()),
+		TimeOffset: tz,
 	}
 }
